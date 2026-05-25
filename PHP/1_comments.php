6232233feca55bc67cfcc00064eed1e0b9a2f185
Muhammad Hamza Habib <mhamzahabib8@gmail.com>
2026-05-25 05:33:55 +0500

    started the php

---
+++ PHP/1_comments.php
@@ -0,0 +1,26 @@
+<?php
+    echo "Hello World!";
+    echo "<br>";
+    echo "This is the start of learning PHP.";
+
+    // echo "this is the comment";
+
+    /*
+    echo "this is multiple line comment";
+    echo "this is multiple line comment";   
+    echo "this is multiple line comment";
+    */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <button>Click Me</button>
+</body>
+</html>
