60fbe25d4f05a293707b044f65840a5a425b7488
Muhammad Hamza Habib <mhamzahabib8@gmail.com>
2026-05-27 00:03:21 +0500

    react  app file

---
+++ React/my-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import Food from "./Food.jsx";
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+
+
+function App() {
+  return(
+    <>
+      <Header />
+      <Food />
+      <Footer />
+    </>
+  );
+}
+export default App
