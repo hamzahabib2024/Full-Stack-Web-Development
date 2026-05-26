4ee737e37a8f949404d0bf6e3f66f7d321f2c178
Muhammad Hamza Habib <mhamzahabib8@gmail.com>
2026-05-27 00:02:48 +0500

    food react component

---
+++ React/my-react-app/src/Food.jsx
@@ -0,0 +1,11 @@
+
+function Food(){
+    return(
+            <ul>
+                <li>Pizza</li>
+                <li>Burger</li>
+                <li>Salad</li>
+            </ul>
+    );
+}
+export default Food
