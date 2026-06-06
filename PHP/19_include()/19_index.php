aa248a2586503ce1967679e9edd46a3b632499fa
Muhammad Hamza Habib <mhamzahabib8@gmail.com>
2026-06-06 21:34:36 +0500

    index for the simple page in php

---
+++ PHP/19_include()/19_index.php
@@ -0,0 +1,22 @@
+<?php
+    include("19_header.html");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    This is the home page of our website.
+    We have included the header file using include() function. 
+    The header file contains the navigation bar of our website. 
+    We can reuse the header file on multiple pages of our website. 
+    If we want to make any changes to the navigation bar, we only need to make changes in the header file and it will be reflected on all the pages that include the header file.
+</body>
+</html>
+<?php
+    include("19_footer.html");
+?>
