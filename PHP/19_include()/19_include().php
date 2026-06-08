f1403e505225963e4e902cec697d8e4241638e5a
Muhammad Hamza Habib <mhamzahabib8@gmail.com>
2026-06-08 18:31:29 +0500

    include() in php

---
+++ PHP/19_include()/19_include().php
@@ -0,0 +1,9 @@
+<?php
+    //include():
+          // the include() copies the content of a file (php/html/text) and includes it in your php file.
+          // the benefit of using include() is that : sections of our website become reusable.
+        //    and changes only need to be made in one place.
+
+    // syntax: include("filename.php");
+
+?>
