cfd3b6c534465b9650f57ce35f632ead56c2c7f9
Muhammad Hamza Habib <mhamzahabib8@gmail.com>
2026-06-08 18:31:15 +0500

    location.php for the learning include() in php

---
+++ PHP/19_include()/19_location.php
@@ -0,0 +1,19 @@
+<?php
+    include("19_header.html");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    This is the location page of our website.
+    We are located at 123 Main Street, Islamabad, Pakistan.
+</body>
+</html>
+<?php
+    include("19_footer.html");
+?>
