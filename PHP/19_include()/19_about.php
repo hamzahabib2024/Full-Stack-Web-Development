12378058057c4af9a97ec11942520e2e793262fa
Muhammad Hamza Habib <mhamzahabib8@gmail.com>
2026-06-08 18:30:42 +0500

    about.php for the learning include() in php

---
+++ PHP/19_include()/19_about.php
@@ -0,0 +1,19 @@
+<?php
+    include("19_header.html");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    This is the about page of our website.
+    you can contact us at mhamzahabib@gmail.com
+</body>
+</html>
+<?php
+    include("19_footer.html");
+?>
