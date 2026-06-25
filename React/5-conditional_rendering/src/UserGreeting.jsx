72bb71e201abbe7c22cd3e27560c7773ff306ee8
Muhammad Hamza Habib <mhamzahabib8@gmail.com>
2026-06-25 10:38:50 +0500

    feat: implement UserGreeting component for conditional rendering

---
+++ React/5-conditional_rendering/src/UserGreeting.jsx
@@ -0,0 +1,10 @@
+function UserGreeting(props){
+
+    if(props.isLoggedIn){
+        return <h2 className="welcome-message"> Wellcome {props.username}</h2>
+    }
+    else{
+        return <h2 className="login-prompt"> Please log in!</h2>
+    }
+}
+export default UserGreeting
