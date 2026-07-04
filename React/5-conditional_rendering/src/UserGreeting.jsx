3dd53fd7221f7a19e480c593e547a257ef37c680
Muhammad Hamza Habib <mhamzahabib8@gmail.com>
2026-07-05 04:50:54 +0500

    "use of ternary operator for conditional rendering" in react

--- React/5-conditional_rendering/src/UserGreeting.jsx
+++ React/5-conditional_rendering/src/UserGreeting.jsx
@@ -1,10 +1,31 @@
+// import PropTypes from "prop-types";
+
 function UserGreeting(props){
 
-    if(props.isLoggedIn){
-        return <h2 className="welcome-message"> Wellcome {props.username}</h2>
-    }
-    else{
-        return <h2 className="login-prompt"> Please log in!</h2>
-    }
+    // if(props.isLoggedIn){
+    //     return <h2 className="welcome-message"> Wellcome {props.username}</h2>
+    // }
+    // else{
+    //     return <h2 className="login-prompt"> Please log in!</h2>
+    // }
+
+
+    // using ternary operator:
+
+    const welcomeMessage = <h2 className="welcome-message"> Wellcome {props.username}</h2>
+    const loginPrompt = <h2 className="login-prompt"> Please log in!</h2>
+
+    return(props.isLoggedIn ? welcomeMessage : loginPrompt);
 }
+
+// UserGreeting.propTypes = {
+//     isLoggedIn: PropTypes.bool,
+//     username: PropTypes.string,
+// };
+
+UserGreeting.defaultProps = {
+    isLoggedIn: false,
+    username: "Guest",
+};
+
 export default UserGreeting
