df0d096c2c2eb9caf94af319568090e04bbcf713
Muhammad Hamza Habib <mhamzahabib8@gmail.com>
2026-07-05 04:51:05 +0500

    Add comments to explain conditional rendering in App component

--- React/5-conditional_rendering/src/App.jsx
+++ React/5-conditional_rendering/src/App.jsx
@@ -1,9 +1,14 @@
+// conditional rendering:
+      // it allows us to control what gets rendered on the screen based on certain conditions. 
+      // (show, hide, or change component/content based on specific criteria)
+
 import UserGreeting from "./UserGreeting";
 function App(){
 
   return(
     <>
       <UserGreeting isLoggedIn={true} username="Hamza Habib" />
+      {/* <UserGreeting /> */}
     </>
   );
 }
