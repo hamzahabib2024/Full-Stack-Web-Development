070cf89093979b7ee800c1c50661500374e4e730
Muhammad Hamza Habib <mhamzahabib8@gmail.com>
2026-07-09 01:09:17 +0500

    Add age and employment status management to MyComponent

--- React/8-useState_hook/src/MyComponent.jsx
+++ React/8-useState_hook/src/MyComponent.jsx
@@ -3,9 +3,17 @@ import React, { useState } from 'react';
 function MyComponent() {
 
     const [name, setName] = useState("Guest");
+    const[age, setAge] = useState(0);
+    const[isEmployed, setIsEmployed] = useState(false);
 
     const updateName = () => {
         setName("Hamza Habib");
+    }
+    const updateAge = () => {
+        setAge(age + 1);
+    }
+    const updateEmploymentStatus = () => {
+        setIsEmployed(!isEmployed);
     }
 
 
@@ -15,6 +23,10 @@ function MyComponent() {
         
             <p>Name : {name}</p>
             <button onClick={updateName}>Change Name</button>
+            <p>Age : {age}</p>
+            <button onClick={updateAge}>Increment Age</button>
+            <p>Is Employed : {isEmployed ? "Yes" : "No"}</p>
+            <button onClick={updateEmploymentStatus}>Toggle Employment Status</button>
         </div>
     );
 }
