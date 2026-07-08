f4e6725b15b48547c515c1c31b61eec7041fce53
Muhammad Hamza Habib <mhamzahabib8@gmail.com>
2026-07-09 01:10:54 +0500

    update App component to include Counter component

--- React/8-useState_hook/src/App.jsx
+++ React/8-useState_hook/src/App.jsx
@@ -8,13 +8,15 @@
           // [name, setName]
 
 import MyComponent from "./MyComponent.jsx";
+import Counter from "./Counter.jsx";
 
 function App(){
 
 
   return(
     <>
-      <MyComponent />
+      {/* <MyComponent /> */}
+      <Counter />
     </>
   );
 }
